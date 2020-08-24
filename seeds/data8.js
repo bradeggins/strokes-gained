ba65c891285ab29222bdc4db64be63f43bf2a710
Bradley Eggins <brad.eggins@gmail.com>
2020-08-24 16:59:53 +1200

    Add seeds for sgData

---
+++ seeds/data8.js
@@ -0,0 +1,13 @@
+const path = require('path');
+ 
+const seedFile = require('knex-seed-file');
+ 
+exports.seed = function(knex, Promise) {
+  knex('data').del()
+    .then(() => seedFile(knex, path.resolve('../strokes-gained/data/sgData8.csv'), 'data',
+    {
+      columnSeparator: ',',
+      ignoreFirstLine: true,
+      mapTo: ['shotFrom', 'distToHole', 'strokesToHole']
+    }));
+};
