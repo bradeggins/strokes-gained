99613cac696287fcfab032956b2baa6d9978eae7
Bradley Eggins <brad.eggins@gmail.com>
2020-09-07 21:53:02 +1200

    Add testing for submit button enabled/disabled

--- client/components/Contact.test.jsx
+++ client/components/Contact.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, screen, render } from '@testing-library/react'
-
+import {ReactDOM} from 'react-dom'
 import Contact from './contact'
 
 describe('Contact component', () => {
@@ -14,5 +14,30 @@ describe('Contact component', () => {
         let heading = screen.getByRole('heading').textContent
         expect(heading).toContain('Contact Us')
     })
+
+    test('Check form submit is enabled after email and human check completed', () => {
+        render(<Contact />)
+        let emailForm = screen.getByLabelText('Email')
+        let humanCheck = screen.getByLabelText(/Are you a human/i)
+
+        fireEvent.change(emailForm, {target: {value: 'bob@gmail.com'}})
+        fireEvent.change(humanCheck, {target: {value: 7}})
+
+        let disabledButton = screen.getByRole('button', {name: /Submit/i}).disabled
+             
+        expect(disabledButton).toBeFalsy()
+    })
+
+    test('Check submit disable when only email completed', () => {
+        render(<Contact />)
+        let emailForm = screen.getByLabelText('Email')
+        fireEvent.change(emailForm, {target: {value: 'bob@gmail.com'}})
+       
+        let disabledButton = screen.getByRole('button', {name: /Submit/i}).disabled
+             
+        expect(disabledButton).toBeTruthy()
+
+    })
+
 })
 
