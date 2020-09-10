1bb863be5fe36a5d8577034771a905be4c612ce4
Bradley Eggins <brad.eggins@gmail.com>
2020-09-10 17:40:47 +1200

    Fix typo in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -48,7 +48,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.end.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
